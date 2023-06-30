734170c2c03fa98eb71906f9dbd38c5a522d03e2
Andrew Lyons <andrew.lyons@caplin.com>
2023-06-30 17:17:46 +0100

    Spike for a computedFn with support for promises - think the current concept would work but wouldn't follow transactions correctly. I wonder if using intercept or observe with a computed behind the scenes would work better

--- src/state/Account.ts
+++ src/state/Account.ts
@@ -44,6 +44,51 @@ const Balances = configureCollection<Balance, YYYYMM, BalanceSnapshotIn>({
   sort: (a, b) => a.value - b.value
 })
 
+const computedAsyncFn = (getValue) => {
+  const cache = new DeepMap()
+  return (...params) => {
+    if (cache.has(params)) {
+      cache.get(params).current
+    }
+
+    const observableValue = observable({
+      current: getValue(...params)
+    })
+
+    let disposer
+
+    // API not quite right here, maybe use an atom?
+    onBecomeObserved(observableValue, 'current', () => {
+      disposer = reaction(
+        () => {
+          // will this run the first time and result in the value getting set twice?
+          return getValue(...params)
+        },
+        valueOrPromise => {
+          if (valueOrPromise instanceof Promise) {
+            valuePromise.then((newValue) => {
+              // Is this enough to trigger all the observers to
+              observableValue.current = newValue
+            }).catch(() => {
+              // how to handle an error? Ignore?
+              // Blank down? Maybe a config option for 'error value'
+            })
+          } else {
+            observableValue.current = valueOrPromise
+          }
+        }
+      )
+    })
+
+    onBecomeUnobserved(observableValue, 'current', () => {
+      disposer()
+      cache.delete(params)
+    })
+
+    return observableValue.current
+  }
+}
+
 export class Account {
   store: Store
   balances: InstanceType<typeof Balances>
@@ -103,7 +148,6 @@ export class Account {
 
     this.balances.restore(balances)
   }
-
 
   get owner() {
     return this.store.people.get(this.ownerId)
