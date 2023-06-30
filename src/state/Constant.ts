675c155a15d17f3e34e446fb522d4df91f4c9929
Andrew Lyons <andrew.lyons@caplin.com>
2023-06-30 17:12:13 +0100

    Prototyping some work on the Constant functionality

--- src/state/Constant.ts
+++ src/state/Constant.ts
@@ -1,26 +1,31 @@
-import { YYYY } from '../utils/date';
+import { YYYY, YYYYMM } from '../utils/date';
 import { Store } from './Store';
 
-enum ConstantType {
-  NUMBER = 'number',
-  DATE = 'date'
-}
-
-type ValueType<T extends ConstantType> =
-  T extends ConstantType.NUMBER ? number :
-  T extends ConstantType.DATE ? YYYY :
-  never
-
-export class Constant {
+class BaseConstant<T> {
   store: Store
+  value: T
 
-  type: ConstantType
-  value: ValueType<typeof this.type>
-
-  constructor(store: Store, { type, value }: Pick<Constant, 'type' | 'value'>) {
+  constructor(store: Store, { value }: { value: T }) {
     this.store = store
 
-    this.type = type
     this.value = value
   }
 }
+
+export class NumberConstant extends BaseConstant<number> {}
+export class YYYYMMConstant extends BaseConstant<YYYYMM> {}
+export class YYYYConstant extends BaseConstant<YYYY> {}
+export class DurationConstant extends BaseConstant<number> {}
+
+export type Constant = NumberConstant | YYYYMMConstant | YYYYConstant | DurationConstant
+
+/* TODO
+
+- models for different values e.g. NumberValue, DateValue, YearValue, DurationValue(is this just a NumberValue?)
+- a value contains an array of 'operations' and 'operands'
+- wherever user needs to enter a number they can instead enter 'advanced mode' where they can configure the operands/operators
+- .value prop which runs the operation chain to get the value
+- operation chain could just be a simple single number (which is how basic mode is implemented)
+- every value in the app uses one of these model classes rather than the primitive
+
+*/
